d0a85255c47082462c9627b3abc3b982517b21bb
Anvi <anvikudaraya417@gmail.com>
2025-10-30 02:12:43 -0700

    added more project stuff

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,7 +1,11 @@
 const PROJECTS = [
+  { title: 'Music Recomendation System', desc: 'Used Spotify Dataset to create music recomdnation system using NLP and ML', tags: ['NLP', 'Machine Learning', 'Python'], link: '#', image: "public/logos/cruze.png"},
   { title: 'Cruze - Campus Carpooling', desc: 'Web app for secure ride sharing across campus communities', tags: ['React', 'Flask', 'SQL'], link: '#', image: "public/logos/cruze.png"},
   { title: 'AI Irrigation Project', desc: 'RL ML project that reduces freshwater consumption on almond orchards', tags: ['Python', 'RL', 'CouchDB'], link: '#', image: "public/logos/almond.png" },
-  { title: 'AI Tic-Tac-Toe', desc: 'AI vs human tic tac toe', tags: ['React', 'TensorFlow', 'API'], link: '#' },
-  { title: 'Range Search Structures', desc: 'Greedy trees for fast multi-metric queries', tags: ['Algorithms', 'Theory', 'Python'], link: '#' }
+  { title: 'Range Query on Product Metrics', desc: 'Designed two tree based algorithms to do range search on multiple metrics', tags: ['Algorithms', 'Data Strcutures', 'Research'], link: '#', image: "public/logos/research.png" },
+  { title: 'Regex PDF Extractor', desc: 'Published app on gumroad that extracts important date and statistics from PDF files', tags: ['Algorithms', 'Regex', 'Python'], link: '#', image: "public/logos/gumroad.png" },
+  { title: 'AI Tic-Tac-Toe', desc: 'AI vs human tic tac toe', tags: ['React', 'TensorFlow', 'API'], link: '#', image: "public/logos/tictactoe.png" },
+  { title: 'Wellness Website', desc: 'First hackathon project, basic website to find wellness resources', tags: ['HTML/CSS', 'JavaScript', 'Google API'], link: '#', image: "public/logos/hackmerced.png"  }
+  
 ]
 export default PROJECTS
