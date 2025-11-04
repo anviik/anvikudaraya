97a96ebb613535d00a21d29376e295fc7b98a3ec
Anvi <anvikudaraya417@gmail.com>
2025-11-04 01:07:19 -0800

    added project info

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,11 +1,12 @@
 const PROJECTS = [
-  { title: 'Music Recomendation System', desc: 'Used Spotify Dataset to create music recomdnation system using NLP and ML', tags: ['NLP', 'Machine Learning', 'Python'], link: '#', image: "logos/cruze.png"},
-  { title: 'Cruze - Campus Carpooling', desc: 'Web app for secure ride sharing across campus communities', tags: ['React', 'Flask', 'SQL'], link: '#', image: "logos/cruze.png"},
-  { title: 'AI Irrigation Project', desc: 'RL ML project that reduces freshwater consumption on almond orchards', tags: ['Python', 'RL', 'CouchDB'], link: '#', image: "logos/almond.png" },
-  { title: 'Range Query on Product Metrics', desc: 'Designed two tree based algorithms to do range search on multiple metrics', tags: ['Algorithms', 'Data Strcutures', 'Research'], link: '#', image: "logos/research.png" },
-  { title: 'Regex PDF Extractor', desc: 'Published app on gumroad that extracts important date and statistics from PDF files', tags: ['Algorithms', 'Regex', 'Python'], link: '#', image: "logos/gumroad.png" },
+  { title: 'Music Recomendation System', desc: 'Used Spotify Dataset to create music recomdnation system using NLP and ML', tags: ['NLP', 'Machine Learning', 'Python'], link: '#', image: "tech/musicrec.png"},
+  { title: 'Cruze - Campus Carpooling', desc: 'Web app for secure ride sharing across campus communities', tags: ['React', 'Flask', 'SQL'], link: 'https://ucm-rideshare.vercel.app/', image: "logos/cruze.png"},
+  { title: 'AI Irrigation Project', desc: 'RL ML project that reduces freshwater consumption on almond orchards', tags: ['Python', 'RL', 'CouchDB'], link: 'https://news.ucmerced.edu/news/2025/ai-powered-irrigation-system-offers-opportunities-communications-well-farming', image: "logos/almond.png" },
+  { title: 'Range Query on Product Metrics', desc: 'Designed two tree based algorithms to do range search on multiple metrics', tags: ['Algorithms', 'Data Strcutures', 'Research'], link: 'https://linktr.ee/productmetricgreedyrangetrees', image: "logos/research.png" },
+  { title: 'Regex PDF Extractor', desc: 'Published app on gumroad that extracts important date and statistics from PDF files', tags: ['Algorithms', 'Regex', 'Python'], link: 'https://anvi417.gumroad.com/l/xroej?_gl=1*1i84bt0*_ga*MTUyNzg0MjMwOS4xNzYyMjQ2MDU0*_ga_6LJN6D94N6*czE3NjIyNDYwNTQkbzEkZzEkdDE3NjIyNDYwNzIkajQyJGwwJGgw', image: "logos/gumroad.png" },
+  { title: 'Open Source Contributer', desc: 'Contributed to Eclipse PDE Open Source Project', tags: ['Java', 'Github', 'API'], link: 'https://docs.google.com/document/d/1GenGp9TMjkemrE-EJT0x_iHBzIrjxO4tN0verNu1VwE/edit?usp=sharing', image: "logos/eclipse.png" },
   { title: 'AI Tic-Tac-Toe', desc: 'AI vs human tic tac toe', tags: ['React', 'TensorFlow', 'API'], link: '#', image: "logos/tictactoe.png" },
-  { title: 'Wellness Website', desc: 'First hackathon project, basic website to find wellness resources', tags: ['HTML/CSS', 'JavaScript', 'Google API'], link: '#', image: "public/logos/hackmerced.png"  }
+  { title: 'Wellness Website', desc: 'First hackathon project, basic website to find wellness resources', tags: ['HTML/CSS', 'JavaScript', 'Google API'], image: "public/logos/hackmerced.png"  }
   
 ]
 export default PROJECTS
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -41,7 +41,7 @@ export default function Hero() {
               <FaLinkedin className="w-8 h-8" />
             </a>
             <a
-              href="/path-to-your-resume.pdf"
+              href="public/AnviKudarayaNov2025.pdf"
               target="_blank"
               rel="noopener noreferrer"
               className="p-3 rounded-full border border-white/10 hover:bg-white/10 transition-colors"
